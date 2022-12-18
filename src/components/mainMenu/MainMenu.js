534579542f23af05826783e0c5ad32c63dbf017d
Bohdan Datsenko <bohdan.datsenko@sovos.com>
2022-12-19 00:14:22 +0200

    fix of issues displayed in console

--- src/components/mainMenu/MainMenu.js
+++ src/components/mainMenu/MainMenu.js
@@ -4,23 +4,26 @@ import {
     Card, Divider,
     Drawer,
     Grid,
-    IconButton,
+    IconButton, Portal,
     useTheme
 } from "@mui/material";
 import useStyles from "./useStyles";
 import {FeedOutlined, LightModeOutlined, LoginOutlined, MenuOutlined, SettingsOutlined} from "@mui/icons-material";
 import SearchComponent from "./SearchComponent";
-import {useEffect, useState} from "react";
-import {createPortal} from "react-dom";
+import {useEffect, useRef, useState} from "react";
 
 const MainMenu = () => {
     const theme = useTheme()
     const customStyles = useStyles(theme)
 
     const [isDrawerOpen, setIsDrawerOpen] = useState(false)
-    const [isDocumentMounted, setIsDocumentMounted] = useState(false)
+    const [isMounter, setIsMounted] = useState(false)
+    const portalRef = useRef()
 
-    useEffect(() => setIsDocumentMounted(true), [])
+    useEffect(() => {
+        portalRef.current = document.getElementById("header-toolbar")
+        setIsMounted(true)
+    }, [])
 
     const menuItems = [
         {content: 'Всі статті', icon: <FeedOutlined color={'primary'}/>},
@@ -29,19 +32,16 @@ const MainMenu = () => {
         {content: 'Вхід', icon: <LoginOutlined color={'primary'}/>}]
 
     const renderMenuItems = items => items.map(item =>
-        <Button variant={'text'} size={'large'} sx={customStyles.mainMenuItem} endIcon={item.icon}>
+        <Button key={item.content} variant={'text'} size={'large'} sx={customStyles.mainMenuItem} endIcon={item.icon}>
                 {item.content}
-            </Button>)
+        </Button>)
 
     const mobileMenuButton = <IconButton onClick={() => setIsDrawerOpen(true)}>
         <MenuOutlined sx={customStyles.menuButton} fontSize={'large'} />
     </IconButton>
-    const toolbarNode = document.getElementById("header-toolbar")
-
-    const menuButtonPortal = isDocumentMounted && createPortal(mobileMenuButton, toolbarNode)
 
     return <Card sx={{borderRadius: 0}}>
-        {menuButtonPortal}
+        {isMounter && <Portal children={mobileMenuButton} container={portalRef.current}/>}
         <Drawer anchor={'left'} open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
             <Box style={customStyles.sideMenuContainer}>
                 <SearchComponent />
@@ -56,7 +56,7 @@ const MainMenu = () => {
                 </Box>
             </Grid>
             <Grid width={'50%'}>
-                <Box style={customStyles.menuSearchSection} menuSearchSection>
+                <Box style={customStyles.menuSearchSection}>
                     <SearchComponent/>
                 </Box>
             </Grid>
