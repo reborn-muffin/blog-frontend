0fa4e2150277d5a25b994969d93bcd9baf297553
Bohdan Datsenko <reborn.muffin@gmail.com>
2023-01-29 13:09:35 -0800

    replace json-server uri to java service uri

--- src/components/comments/commentsAPI.js
+++ src/components/comments/commentsAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 class CommentsAPI {
     static fetchAll = async () => {
-        const response = await axios.get("http://localhost:3000/comments")
+        const response = await axios.get("http://localhost:8080/api/v1/comments")
         return response.data
     }
 }
--- src/components/pageContent/articlesAPI.js
+++ src/components/pageContent/articlesAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 class ArticlesAPI {
     static fetchAll = async () => {
-        const response = await axios.get("http://localhost:3000/articles")
+        const response = await axios.get("http://localhost:8080/api/v1/articles")
         return response.data
     }
 }
